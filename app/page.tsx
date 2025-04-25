3d98eab7afb6787b366a8712caed3b6d48f2eff0
Jayanth Adhitya <jayanthadhitya@gmail.com>
2025-04-25 14:32:09 +0530

    Fixed Deployment issue [3]

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+//import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import dynamic from "next/dynamic";
+
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
 
 const Home = () => {
   return (
